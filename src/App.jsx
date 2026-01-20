e5d4b1723f6621a53ddab8d5d2bba99fc4fce5ee
Pedro Díaz <pedrosdhdez@gmail.com>
2026-01-20 16:52:35 +0000

    Ejercicio dia 27: Uso de PROPS

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,138 @@
 import { useState, useEffect } from "react";
-import Pokedex from "./components/PokemonCard.jsx";
+import PokemonCard from "./components/PokemonCard.jsx";
+
+// function App() {
+//   const [name, setName] = useState(`Pedro Díaz`);
+//   const age = 30;
+//   const isStudent = true;
+//   const avatar = `https://i.pravatar.cc/300`;
+//   const [contador, setContador] = useState(0);
+//   const [text, setText] = useState("");
+
+//   const [pokeTextInput, setPokeTextInput] = useState("");
+//   const [busqueda, setBusqueda] = useState("pikachu");
+
+//   const manejarBusqueda = () => {
+//     setBusqueda(pokeTextInput.toLowerCase());
+//   };
+
+//   useEffect(() => {
+//     document.title = `Contador: ${contador}`;
+//     console.log(`Me he ejecutado porque el contador cambio`);
+//   }, [contador]);
+
+//   const inputText = (event) => {
+//     const inputedText = event.target.value;
+//     setText(inputedText);
+//   };
+
+//   const upCounter = () => {
+//     setContador(contador + 1);
+//   };
+
+//   const downCounter = () => {
+//     setContador(contador - 1);
+//   };
+
+//   const driveClick = () => {
+//     if (name === "Gokú") {
+//       setName("Pedro Díaz");
+//     } else {
+//       setName("Gokú");
+//     }
+//     console.log("Nombre cambiado a:", name);
+//     console.log("¡Auch! Me has hecho click");
+//   };
+
+//   const masterFunction = () => {
+//     upCounter();
+//     driveClick();
+//   };
+
+//   return (
+//     <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+//       <h1>Perfil de {name}</h1>
+
+//       <h2>Contador mágico</h2>
+//       <h3 style={{ fontSize: "4rem", margin: "10px" }}>{contador}</h3>
+//       <img
+//         src={avatar}
+//         alt="Foto de perfil"
+//         width="150"
+//         style={{ borderRadius: "50%" }}
+//       />
+//       <p>Edad: {age} años</p>
+//       <p>
+//         Estado:
+//         {isStudent ? "📚 Estudiando a tope" : " 🏖️ De vacaciones"}
+//       </p>
+//       <small>Nacimiento aprox: {2026 - age}</small>
+//       <button
+//         onClick={masterFunction}
+//         style={{ padding: "10px 20px", fontSize: "16px" }}
+//       >
+//         Púlsame para aumentar el contador
+//       </button>
+//       <button
+//         onClick={downCounter}
+//         style={{ padding: "10px 20px", fontSize: "16px" }}
+//       >
+//         Púlsame para restar el contador
+//       </button>
+
+//       <h2>El espejo mágico</h2>
+
+//       <input
+//         type="text"
+//         value={text}
+//         onChange={inputText}
+//         style={{ padding: "10px", fontSize: "18px" }}
+//       />
+//       <h3>Has escrito:</h3>
+//       <p style={{ color: "blue", fontSize: "30px", fontWeight: "bold" }}>
+//         {text}
+//       </p>
+
+//       <h2>Mira la pestaña del navegador ⬆️</h2>
+//       <h3>{contador}</h3>
+
+//       <div>
+//         <h1>Buscador de Pókemon</h1>
+//         <input
+//           type="text"
+//           value={pokeTextInput}
+//           onChange={(e) => setPokeTextInput(e.target.value)}
+//           style={{ padding: "10px", fontSize: "16px" }}
+//         />
+//         <button
+//           onClick={manejarBusqueda}
+//           style={{
+//             padding: "10px 20px",
+//             marginLeft: "10px",
+//             fontSize: "16px",
+//             cursor: "pointer",
+//           }}
+//         >
+//           Buscar
+//         </button>
+//       </div>
+//       <Pokedex nombrePokemon={busqueda} />
+//     </div>
+//   );
+// }
 
 function App() {
-  const [name, setName] = useState(`Pedro Díaz`);
-  const age = 30;
-  const isStudent = true;
-  const avatar = `https://i.pravatar.cc/300`;
-  const [contador, setContador] = useState(0);
-  const [text, setText] = useState("");
-
-  const [pokeTextInput, setPokeTextInput] = useState("");
-  const [busqueda, setBusqueda] = useState("pikachu");
-
-  const manejarBusqueda = () => {
-    setBusqueda(pokeTextInput.toLowerCase());
-  };
-
-  useEffect(() => {
-    document.title = `Contador: ${contador}`;
-    console.log(`Me he ejecutado porque el contador cambio`);
-  }, [contador]);
-
-  const inputText = (event) => {
-    const inputedText = event.target.value;
-    setText(inputedText);
-  };
-
-  const upCounter = () => {
-    setContador(contador + 1);
-  };
-
-  const downCounter = () => {
-    setContador(contador - 1);
-  };
-
-  const driveClick = () => {
-    if (name === "Gokú") {
-      setName("Pedro Díaz");
-    } else {
-      setName("Gokú");
-    }
-    console.log("Nombre cambiado a:", name);
-    console.log("¡Auch! Me has hecho click");
-  };
-
-  const masterFunction = () => {
-    upCounter();
-    driveClick();
-  };
-
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1>Perfil de {name}</h1>
+    <div>
+      <h1>Pokédex (Usando Props)</h1>
 
-      <h2>Contador mágico</h2>
-      <h3 style={{ fontSize: "4rem", margin: "10px" }}>{contador}</h3>
-      <img
-        src={avatar}
-        alt="Foto de perfil"
-        width="150"
-        style={{ borderRadius: "50%" }}
-      />
-      <p>Edad: {age} años</p>
-      <p>
-        Estado:
-        {isStudent ? "📚 Estudiando a tope" : " 🏖️ De vacaciones"}
-      </p>
-      <small>Nacimiento aprox: {2026 - age}</small>
-      <button
-        onClick={masterFunction}
-        style={{ padding: "10px 20px", fontSize: "16px" }}
-      >
-        Púlsame para aumentar el contador
-      </button>
-      <button
-        onClick={downCounter}
-        style={{ padding: "10px 20px", fontSize: "16px" }}
-      >
-        Púlsame para restar el contador
-      </button>
+      <PokemonCard pokemonName="Pikachu" pokemonType="Eléctrico" />
 
-      <h2>El espejo mágico</h2>
+      <PokemonCard pokemonName="Charmander" pokemonType="Fuego" />
 
-      <input
-        type="text"
-        value={text}
-        onChange={inputText}
-        style={{ padding: "10px", fontSize: "18px" }}
-      />
-      <h3>Has escrito:</h3>
-      <p style={{ color: "blue", fontSize: "30px", fontWeight: "bold" }}>
-        {text}
-      </p>
+      <PokemonCard pokemonName="Squirtle" pokemonType="Agua" />
 
-      <h2>Mira la pestaña del navegador ⬆️</h2>
-      <h3>{contador}</h3>
-
-      <div>
-        <h1>Buscador de Pókemon</h1>
-        <input
-          type="text"
-          value={pokeTextInput}
-          onChange={(e) => setPokeTextInput(e.target.value)}
-          style={{ padding: "10px", fontSize: "16px" }}
-        />
-        <button
-          onClick={manejarBusqueda}
-          style={{
-            padding: "10px 20px",
-            marginLeft: "10px",
-            fontSize: "16px",
-            cursor: "pointer",
-          }}
-        >
-          Buscar
-        </button>
-      </div>
-      <Pokedex nombrePokemon={busqueda} />
+      <PokemonCard pokemonName="Bulbasaur" pokemonType="Planta/Veneno" />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,60 +1,73 @@
 import { useState, useEffect } from "react";
 
-function Pokedex({ nombrePokemon }) {
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
-    if (!nombrePokemon) return;
+// function Pokedex({ nombrePokemon }) {
+//   const [pokemon, setPokemon] = useState(null);
+//   useEffect(() => {
+//     if (!nombrePokemon) return;
 
-    console.log(`Buscando a ${nombrePokemon}...`);
+//     console.log(`Buscando a ${nombrePokemon}...`);
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("Pokemon no encontrado...");
-      })
-      .then((data) => {
-        console.log("datos recibidos:", data);
-        setPokemon(data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        setPokemon(null);
-      });
-  }, [nombrePokemon]);
+//     fetch(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`)
+//       .then((response) => {
+//         if (response.ok) {
+//           return response.json();
+//         }
+//         throw new Error("Pokemon no encontrado...");
+//       })
+//       .then((data) => {
+//         console.log("datos recibidos:", data);
+//         setPokemon(data);
+//       })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//         setPokemon(null);
+//       });
+//   }, [nombrePokemon]);
 
+//   return (
+//     <div style={{ textAlign: "center", marginTop: "50px" }}>
+//       {pokemon ? (
+//         <div
+//           style={{
+//             border: "1px solid #ccc",
+//             display: "inline-block",
+//             padding: "20px",
+//             borderRadius: "10px",
+//           }}
+//         >
+//           <h2>{pokemon.name}</h2>
+//           <img
+//             src={pokemon.sprites.front_default}
+//             alt={pokemon.name}
+//             style={{ width: "200px" }}
+//           />
+//           <p>
+//             <strong>ID:</strong>
+//             {pokemon.id}
+//           </p>
+//           <p>
+//             <strong>Tipo:</strong>
+//             {pokemon.types[0].type.name}
+//           </p>
+//         </div>
+//       ) : (
+//         <h3>Cargando pokémon...</h3>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Pokedex;
+
+// Ejercicio día 27: PROPS //
+
+function PokemonCard({ pokemonName, pokemonType }) {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      {pokemon ? (
-        <div
-          style={{
-            border: "1px solid #ccc",
-            display: "inline-block",
-            padding: "20px",
-            borderRadius: "10px",
-          }}
-        >
-          <h2>{pokemon.name}</h2>
-          <img
-            src={pokemon.sprites.front_default}
-            alt={pokemon.name}
-            style={{ width: "200px" }}
-          />
-          <p>
-            <strong>ID:</strong>
-            {pokemon.id}
-          </p>
-          <p>
-            <strong>Tipo:</strong>
-            {pokemon.types[0].type.name}
-          </p>
-        </div>
-      ) : (
-        <h3>Cargando pokémon...</h3>
-      )}
+    <div style={{ border: "1px solid grey", padding: "10px", margin: "10px" }}>
+      <h2>Nombre: {pokemonName}</h2>
+      <p>Elemento: {pokemonType}</p>
     </div>
   );
 }
 
-export default Pokedex;
+export default PokemonCard;
